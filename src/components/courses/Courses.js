5d8b178b858fff76808ea660915e36a338c78fd0
Josue Proano <josue@Josues-MBP.fios-router.home>
2022-05-17 23:33:15 -0400

    mobile layout

--- src/components/courses/Courses.js
+++ src/components/courses/Courses.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./Courses.css";
 import SchoolIcon from "@mui/icons-material/School";
 import CoursesTable from "./CoursesTable";
+import { Grid } from "@mui/material";
 
 function Courses() {
   var i = 0;
@@ -13,14 +14,26 @@ function Courses() {
             <SchoolIcon sx={{ fontSize: 45 }} />
             Relevant Courses
           </h1>
-          <div >
-            <span className="info">Courses Taken at the University of Maryland - Hover Over Course Name for More Info</span>
+          <div>
+            <span className="info">
+              Courses Taken at the University of Maryland - Hover Over Course
+              Name for More Info
+            </span>
           </div>
         </div>
       </div>
-        <div className="tables">
-          <CoursesTable/>
-        </div>
+      <div className="tables">
+        <Grid
+          container
+          direction="row"
+          spacing={{ xs: 1 }}
+          columns={{ xs: 3, sm: 10, md: 12 }}
+        >
+          <Grid item xs={3} sm={10} md={11.2}>
+            <CoursesTable />
+          </Grid>
+        </Grid>
+      </div>
     </div>
   );
 }
